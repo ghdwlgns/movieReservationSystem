acdd6c99f8ae63dcd5a29a30b9697f9a2dd1bcbe
ghdwlgns <atene1408@gmail.com>
2022-11-30 15:51:06 +0900

    apply changes

--- src/main/java/movie_reservation/service/MovieService.java
+++ src/main/java/movie_reservation/service/MovieService.java
@@ -1,2 +1,13 @@
-package movie_reservation.service;public interface MovieService {
+package movie_reservation.service;
+
+import movie_reservation.entities.Movie;
+import movie_reservation.entities.Reservation;
+
+public interface MovieService {
+    void addMovie(Movie movie);
+    void findMovie();
+    void findMovies();
+    void updateMovies();
+    void removeMovie();
+
 }
--- src/main/java/movie_reservation/service/UserService.java
+++ src/main/java/movie_reservation/service/UserService.java
@@ -1,2 +1,13 @@
-package movie_reservation.service;public interface UserService {
+package movie_reservation.service;
+
+import movie_reservation.entities.Reservation;
+import movie_reservation.entities.User;
+
+public interface UserService {
+    void addUser(User user);
+    void findUser();
+    void findUsers();
+    void updateUser();
+    void removeUser();
+    Reservation findReservationByUserName(String userName);
 }
